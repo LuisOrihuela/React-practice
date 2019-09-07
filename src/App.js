a1c92722e2c970a053eafe9e8dce6231a1e38c9c
Luis Orihuela <luis.orihuela20@gmail.com>
2019-09-07 15:16:51 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {userState} from 'react'
+import Tweet from './Tweet';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App () {
+  // const [isRed, setRed] = userState(false);
+  // const [count, setCount] = userState(0);
+  return(
+    <div className="app" >
+      <Tweet name="Luis" message="Bla bla"/>
+      <Tweet name="John" message="Hello"/>
+      <Tweet name="Mary" message="This is a message"/>
+      <Tweet name="Dave" message="Cool"/>
+
     </div>
-  );
+  )
 }
 
 export default App;
